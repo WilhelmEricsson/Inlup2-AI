1c647f60df443860049e98f28dab5df9be30ab05
rubenwilhelmsen <rubenwilhelmsen@live.se>
2019-05-24 12:43:01 +0200

    Tree fixad

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,6 @@
+import processing.core.PImage;
+import processing.core.PVector;
+
 public class Tree extends Sprite {
 
     //PVector position;
@@ -8,9 +11,12 @@ public class Tree extends Sprite {
     //float diameter, radius, m;
     //float m;
 
+    TankProg tp;
+
     //**************************************************
-    Tree(int posx, int posy) {
-        this.img = loadImage("tree01_v2.png");
+    Tree(int posx, int posy, TankProg tp) {
+        this.tp = tp;
+        this.img = tp.loadImage("tree01_v2.png");
         this.position = new PVector(posx, posy);
         //this.hitArea = new PVector(posx, posy); // Kanske inte kommer att användas.
         this.diameter = this.img.width/2;
@@ -34,7 +40,7 @@ public class Tree extends Sprite {
         float minDistance = radius + other.radius;
 
         if (distanceVectMag < minDistance) {
-            println("! collision med en tank [Tree]");
+            System.out.println("! collision med en tank [Tree]");
 
         }
 
@@ -42,22 +48,22 @@ public class Tree extends Sprite {
 
     //**************************************************
     void display() {
-        pushMatrix();
-        translate(this.position.x, this.position.y);
+        tp.pushMatrix();
+        tp.translate(this.position.x, this.position.y);
 
-        fill(204, 102, 0, 100);
+        tp.fill(204, 102, 0, 100);
         int diameter = this.img.width/2;
         //ellipse(this.position.x, this.position.y, diameter, diameter);
-        ellipse(0, 0, diameter, diameter);
+        tp.ellipse(0, 0, diameter, diameter);
         //image(img, this.position.x, this.position.y);
-        image(img, 0, 0);
+        tp.image(img, 0, 0);
 
-        if(debugOn){
-            noFill();
-            stroke(255, 0, 0);
-            ellipse(0, 0, this.diameter(), this.diameter());
+        if(tp.debugOn){
+            tp.noFill();
+            tp.stroke(255, 0, 0);
+            tp.ellipse(0, 0, this.diameter(), this.diameter());
         }
-        popMatrix();
+        tp.popMatrix();
 
     }
 }
