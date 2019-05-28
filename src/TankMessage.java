da5d3cb9a42df460bbb93a26ce48fd81f287708a
rubenwilhelmsen <kuk8EN8drar>
2019-05-28 14:26:53 +0200

    TankMessage update

--- src/TankMessage.java
+++ src/TankMessage.java
@@ -1,17 +1,26 @@
+import processing.core.PVector;
+
 public class TankMessage {
 
     // Tank id
     private int sender;
 
+    // Kanske ändra till int sen istället
     private String message;
+    private PVector position;
 
-    public TankMessage(int sender, String message) {
+    public TankMessage(int sender, String message, PVector position) {
         this.sender = sender;
         this.message = message;
+        this.position = position;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public PVector getPosition() {
+        return position;
     }
 
     public int getSender() {
